668966664dc0a0be1209528434b7530c4863d447
hrishikesh <hshinde166@gmail.com>
2021-12-24 20:29:30 +0530

    Importing a bunch of stuff and getting ready for the actual project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Button, Col, Row, Card, CardBody, Container} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import Icon from "./components/Icon";
 
-function App() {
+const arrayItems = new Array[9].fill("empty"); //Creating an array of 9 elements which are all empty initially
+
+const App = () => {
+
+  const [isCross, setIsCross] = useState(false);// This useState is for changing the state when we enter a cross or a circle in the game
+  const [winMessage, setWinMessage] = useState(""); //This is for displaying the win message when a player wins
+
+  //function to reload the game
+  const gameReload = () => {
+    //
+  }
+
+  //Function to changeitem in the game like cross or circle
+  const changeItem = () => {
+    //
+  }
+ 
+  //Function to check the winner
+  const checkIsWinner = () => {
+    //
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Icon />
       </header>
     </div>
   );
