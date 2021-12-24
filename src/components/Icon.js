644163ae4245cac794bf623c2cc70228e2b95c6b
hrishikesh <hshinde166@gmail.com>
2021-12-25 00:53:56 +0530

    Created the structure of the Icon component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,9 +2,12 @@ import {FaTimes, FaPen, FaRegCircle} from "react-icons/fa"; //Importing the icon
 
 const Icon = (props) => {
 
-    return (
-        <h1><FaTimes className = "icon" /> <FaPen className="icon" /> <FaRegCircle className="icon" /></h1>
-    );
+    switch(props.name){
+        case "circle": return <FaRegCircle className="icons"/>
+        case "cross" : return <FaTimes className="icons" />
+        default: return <FaPen className="icons" />
+    }
+    
 };
 
 export default Icon;
